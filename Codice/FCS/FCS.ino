ec671836f4d588e1c12328490a5dbdff0e6eb27b
Michele <michele.bellucci07@iisgubbio.edu.it>
2024-02-10 16:19:22 +0100

    FCS versione V1.4

--- Codice/FCS/FCS.ino
+++ Codice/FCS/FCS.ino
@@ -2,9 +2,20 @@
 #include <RF24_config.h>
 #include <nRF24L01.h>
 #include <printf.h>
+//pin dei motori tutti devono essere PWM
+#define pinMotore1 5
+#define pinMotore2 6
+#define pinMotore3 9
+#define pinMotore4 10
 
 int val;
 int inserisci=0;
+//OUTPUT che andranno ai motori va da 124 a 250
+int motore1=0;
+int motore2=0;
+int motore3=0;
+int motore4=0;
+
 long tempoPassato=millis();
 
 #define CE_PIN 7
@@ -12,13 +23,17 @@ long tempoPassato=millis();
 // instantiate an object for the nRF24L01 transceiver
 RF24 radio(CE_PIN, CSN_PIN);
 
-uint8_t address[6] = {"1Node"};
+uint8_t address[6] = {"1Node"}; //frequenza di comunicazione
 
-float messaggio[4]={0, 0, 0, 0};
+float messaggio[4]={0, 0, 0, 0}; //valori da 0 a 1023 che vengono inviati dal Joystick
 
 void setup() {
   Serial.begin(115200);
-  pinMode(9, OUTPUT);
+
+  pinMode(pinMotore1, OUTPUT);
+  pinMode(pinMotore2, OUTPUT);
+  pinMode(pinMotore3, OUTPUT);
+  pinMode(pinMotore4, OUTPUT);
 
   if (!radio.begin()) {
     while (true) {Serial.println(F("radio hardware is not responding!!"));}  // hold in infinite loop
@@ -36,19 +51,10 @@ void loop() {
   if (radio.available(&pipe)) {          // is there a payload? get the pipe number that recieved it
     uint8_t bytes = radio.getPayloadSize();  // get the size of the payload
     radio.read(messaggio, sizeof(messaggio));
-    /*          
-    Serial.print(F("Received "));
-    Serial.print(bytes);  // print the size of the payload
-    Serial.print(F(" bytes on pipe "));
-    Serial.print(pipe);  // print the pipe number
-    Serial.print(F(": "));
-    for(int i=0; i< 4; i++){
-      Serial.println(messaggio[i]);
-    }*/
-    Serial.println(messaggio[0]);
   }
+
   if(inserisci <= val){
-    if(millis()-tempoPassato>10){
+    if(millis()-tempoPassato>10){ //aumentiamo di uno ogni 10 millis cosicché si diminuisce il rischio di blocco del motore
       tempoPassato=millis(); 
       inserisci++;
     }
@@ -56,6 +62,8 @@ void loop() {
     inserisci--;
   }
   val = messaggio[0];
-  //val = map(val, 0, 1023, 124, 250);
-  analogWrite(9, inserisci);
+  val = map(val, 0, 1023, 124, 250);
+  scriviIn(9, inserisci);
+  scriviIn(10, inserisci);
+  Serial.println(inserisci);
 }
